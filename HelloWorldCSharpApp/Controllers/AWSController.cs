fedf3b774add1123ff7616bd94fb0433cd6966e0
Jiean Yang <jiean.yang8534@gmail.com>
2022-11-22 15:10:10 +0100

    Add aws credential in AWSController.cs

--- HelloWorldCSharpApp/Controllers/AWSController.cs
+++ HelloWorldCSharpApp/Controllers/AWSController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.EC2;
+using Amazon.Runtime;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +13,16 @@ namespace HelloWorldCSharpApp.Controllers
     [ApiController]
     public class AWSController : ControllerBase
     {
+        private AWSCredentials credentials;
+        private AmazonEC2Client awsEc2Client;
+
+        public AWSController(AWSCredentials credentials, AmazonEC2Client awsEc2Client)
+        {
+            string accessKey = "";
+            string secretKey = "";
+            
+            this.credentials = new BasicAWSCredentials(accessKey, secretKey);
+            this.awsEc2Client = new AmazonEC2Client(credentials, Amazon.RegionEndpoint.EUWest3);
+        }
     }
 }
