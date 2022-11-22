54b50714a4f0136b23fa1bc42c845308999d996c
Jiean Yang <jiean.yang8534@gmail.com>
2022-11-22 11:00:29 +0100

    Create Firend.cs

---
+++ HelloWorldCSharpApp/Friend.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace HelloWorldCSharpApp
+{
+	public class Friend
+    {
+		public string firstName;
+		public string lastName;
+		public string location;
+
+		public Friend()
+		{
+		}
+
+        public Friend(string firstName, string lastName, string location)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.location = location;
+        }
+    }
+}
+
