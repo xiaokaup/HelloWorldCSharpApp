4c79031b79dcc89b62e2f4ecb9692789adc45fd6
Jiean Yang <jiean.yang8534@gmail.com>
2022-11-23 19:06:59 +0100

    Create HelloGrain

---
+++ OrleansBasicApp.Grains/HelloGrain.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace OrleansBasicApp
+{
+	public class HelloGrain : Orleans.Grain, IHello
+	{
+        private readonly ILogger _logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger)
+        {
+            _logger = logger;
+        }
+
+        Task<string> SayHello(string greeting)
+        {
+            _logger.LogInformation("SayHello message received: greeting = '{Greeting}'", greeting);
+            return Task.FromResult($"\n Client said: '{greeting}', so HelloGrain says: Hello!");
+        }
+    }
+}
+
