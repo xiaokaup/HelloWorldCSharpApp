56b3de6f01e71a8af188ac8750d8ac6f9c97abb7
Jiean Yang <jiean.yang8534@gmail.com>
2022-11-23 18:08:33 +0100

    Create HelloGrain.cs

---
+++ HelloOrleansApp/HelloGrain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloOrleansApp
+{
+	public class HelloGrain : Grain, IHelloGrain
+	{
+		public ValueTask<string> SayHello(string greeting)
+		{
+			return ValueTask.FromResult($"Hello, {greeting}");
+		}
+	}
+}
+
